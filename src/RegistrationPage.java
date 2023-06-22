0140873c934e3fee3d1373af1eb0e806fade112c
Roi Yomtovyan <37290526+RoiYomtovyan@users.noreply.github.com>
2023-06-22 15:19:38 +0300

    Revert "master"
    This reverts commit 3a7275b5ffdfe81ad3d594f22c4e8f9a9774cb52.

---
+++ src/RegistrationPage.java
@@ -0,0 +1,64 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+// this class is the elements repository of the registration web page
+
+public class RegistrationPage {
+
+    public static WebElement element;
+
+
+    // the register \ login page link
+    public  static WebElement pressLogin (WebDriver driver){
+        element=driver.findElement(By.cssSelector ("li.notSigned"));
+//        element = driver.findElement(By.xpath("//li//a//span[contains(text(),'כניסה / הרשמה')]"));
+        return element;
+    }
+
+
+    // the not registered Yet link
+    public  static WebElement notRegisteredYet (WebDriver driver){
+        element=driver.findElement(By.className("header-link"));
+        return element;
+    }
+
+    // UserName Field
+    public  static WebElement userName (WebDriver driver){
+        element=driver.findElement(By.cssSelector("input[placeholder=\"שם פרטי\"]"));
+        return element;
+    }
+
+    // mail Field
+    public  static WebElement mailField (WebDriver driver) {
+        element = driver.findElement(By.className("input-mail"));
+        return element;
+    }
+
+    // Password Field
+    public  static WebElement passwordField (WebDriver driver) {
+        element = driver.findElement(By.id("valPass"));
+        return element;
+    }
+
+    // Password validation Field
+    public  static WebElement passwordValidation (WebDriver driver) {
+        element = driver.findElement(By.cssSelector("input[placeholder=\"אימות סיסמה\"]"));
+        return element;
+    }
+
+    // I agree radio button
+    public  static WebElement iAgreeRadioButton (WebDriver driver) {
+        element = driver.findElement(By.cssSelector("label[for=\"register-consent\"]"));
+        return element;
+    }
+
+    // Submit button
+    public  static WebElement Submit (WebDriver driver) {
+        element = driver.findElement(By.cssSelector("button[class=\"db fluid btn btn-theme\"]"));
+        return element;
+    }
+
+
+
+}
