31ff1fab9306a4928aade377465996e2e18ba12b
Roi Yomtovyan <37290526+RoiYomtovyan@users.noreply.github.com>
2023-06-22 12:20:37 +0300

    Update ApiTests.java

--- src/ApiTests.java
+++ src/ApiTests.java
@@ -1,31 +1,17 @@
-
-import com.google.gson.JsonObject;
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
-
-import java.io.FileReader;
 import java.io.IOException;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ApiTests {
-
     String filePath = "C:\\Users\\avivit\\Documents\\GitHub\\BuyMeProject\\config.json";
-
     JSONObject config = jsonReader.parseJSONFile(filePath);
-
     String url = config.getJSONObject("api").get("url").toString();
     String urlSuffix = config.getJSONObject("api").get("urlSuffix").toString();
-    String city = "london";
-
 
     public ApiTests() throws IOException {
     }
-
 
     @Test
     public void
